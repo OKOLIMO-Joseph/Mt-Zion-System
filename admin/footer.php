1724b59f5bfa1ac267f592a61a7b20a6953916b5
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2025-07-10 20:25:19 +0300

    Source code update

--- admin/footer.php
+++ admin/footer.php
@@ -1,6 +1,6 @@
 <div class="setfooter">
 		<footer >
-           <p align="center"><h3 align="center"> &copy; Mt. Zion Church <?php
+           <p align="center"><h3 align="center"> &copy; S Mt. Zion Church <?php
  $date = new DateTime();
  echo $date->format(' Y');
  ?> </h3></p>
